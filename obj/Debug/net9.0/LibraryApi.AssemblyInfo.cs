1e54f952f5032ebd03fd20ad0a67735955bf7f9e
Hajron <ronny.harjo@hotmail.com>
2025-09-05 21:32:33 +0200

    Just incase

--- obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bbbef85b42a886347a03c79b576d3b0b899357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222f3a0bf4c01ea8876365a83186b5aca2b2670b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
