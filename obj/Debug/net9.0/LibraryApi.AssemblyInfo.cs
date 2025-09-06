cef638548e31c27a4de327704f11b11c8fe8611f
Hajron <ronny.harjo@hotmail.com>
2025-09-06 22:23:36 +0200

    Added Author model

--- obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad547679138b0b61f6a8ac8358fa948d37c86601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e45eeb432f40518ae3404ae62a1b7a5d5ba9419")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
