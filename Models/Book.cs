fdb4419f77e1a0a1162824bf215ae6534ec94ee6
Hajron <ronny.harjo@hotmail.com>
2025-09-06 22:31:59 +0200

    Changes to Book model, added author info

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LibraryApi.Models;
 public class Book
@@ -9,9 +10,12 @@ public class Book
     [StringLength(100, ErrorMessage = "Title length can't be more than 100.")]
     public string Title { get; set; } = "";
 
-    [Required(ErrorMessage = "Author is required.")]
-    public string Author { get; set; } = "";
-
     [Range(1910, 2025, ErrorMessage = "Year must be between 1910 and 2024.")]
     public int Year { get; set; }
+
+    [ForeignKey("Author")]
+    public string AuthorId { get; set; } = "";
+    public Author? Author { get; set; }
+
+    
 }
